f616abddf6091b97316462cd668c323311c8555d
路过秋天 <272657997@qq.com>
2024-01-09 16:06:47 +0800

    增加两个单独针对MQ的版本工程

---
+++ src/MQ/Rabbit/MQ.Rabbit.NoSupport.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace Taurus.Plugin.DistributedTransaction
+{
+
+    internal class MQRabbit : MQ
+    {
+ 
+        public override MQType MQType
+        {
+            get
+            {
+                return MQType.Rabbit;
+            }
+        }
+        public MQRabbit(string mqConn)
+        {
+            
+        }
+
+        public override bool Publish(MQMsg msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool PublishBatch(List<MQMsg> msgList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Listen(string queueNameOrGroupName, OnReceivedDelegate onReceivedDelegate, string bindExNameOrTopicName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
